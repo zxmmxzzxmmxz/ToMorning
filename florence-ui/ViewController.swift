5bfc37fc0dc2102b724cd35e3bfb7c0f8a798ec8
florence Huang <tha58@asb9700-e02.csil.sfu.ca>
2015-11-16 21:56:32 -0800

    Initialization

---
+++ florence-ui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  florence-ui
+//
+//  Created by florence Huang on 2015-11-16.
+//  Copyright (c) 2015 florence Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
